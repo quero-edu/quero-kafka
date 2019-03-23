5dee078cd3ed7da6e7096cc579a4a11ee12e279c
Vinicius Souza <viniciusjssouza@gmail.com>
2019-03-23 17:44:00 -0300

    fix docker-compose file location

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+set -e
+
+if [ -d "$HOME/.kafka/quero-kafka" ]; then
+  echo "Kafka local environment already installed. If you want to reinstall it, delete the '.kafka/quero-kafka' directory"
+  exit 0
+fi
+
 echo "Fetching files from remote repository..."
 mkdir -p $HOME/.kafka
 cd $HOME/.kafka
 git clone https://github.com/quero-edu/quero-kafka.git
+cd $HOME/.kafka/quero-kafka
 chmod +x kafka-up
 
 echo "Adding executable to path..."
-echo 'export PATH=${PATH}:${HOME}/.kafka' >>  $HOME/.bashrc
+grep -qxF '.kafka/quero-kafka' $HOME/.bashrc || echo "export PATH=\$PATH:$HOME/.kafka/quero-kafka" >>  $HOME/.bashrc
 source $HOME/.bashrc
 
 echo "Installing kafkacat..."
