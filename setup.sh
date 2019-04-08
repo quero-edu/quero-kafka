08df06a682e269af3c37fd6a48858773c007212e
luiz.rolim <luiz.rolim@redealumni.com>
2019-04-08 10:53:48 -0300

    mac setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+is_mac=false
+if [ "$(uname)" == "Darwin" ]; then
+  is_mac=true
+fi
+
+
 if [ -d "$HOME/.kafka/quero-kafka" ]; then
   echo "Kafka local environment already installed. If you want to reinstall it, delete the '.kafka/quero-kafka' directory"
   exit 0
@@ -15,18 +21,39 @@ chmod +x kafkaup
 
 echo "Adding executable to path..."
 grep 'quero-kafka' $HOME/.bashrc || echo "export PATH=\$PATH:$HOME/.kafka/quero-kafka" >>  $HOME/.bashrc
-source $HOME/.bashrc
+
 if [[ -e $HOME/.zshrc ]]
 then
   grep 'quero-kafka' $HOME/.zshrc || echo "export PATH=\$PATH:$HOME/.kafka/quero-kafka" >>  $HOME/.zshrc
 fi
 
+#!/usr/bin/env bash
+
+echo "Adding DATA_PATH variable"
+
+if [ is_mac ]; then
+  echo "export DATA_PATH=/private/var/data" >>  $HOME/.bashrc
+  if [[ -e $HOME/.zshrc ]]; then
+    echo "export DATA_PATH=/private/var/data" >>  $HOME/.zshrc
+  fi
+else
+    echo "export DATA_PATH=/var/data" >>  $HOME/.bashrc
+fi
+
+echo "rebuild bashrc"
+
+source $HOME/.bashrc
+
 
 echo "Installing kafkacat..."
-sudo apt-get -y install kafkacat
+if [ is_mac ]; then
+  brew install kafkacat
+else
+  sudo apt-get -y install kafkacat
+fi
+
 
 echo "Configuring docker network"
 docker network create querobolsa_default || true
 
 echo "Done! Run 'kafkaup' and enjoy Kafka!"
-
